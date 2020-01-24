8ffca49f1077d83064ec3b62977d27e8644a461b
William <william.santos@hotmail.co.uk>
2020-01-24 11:52:13 +0000

    wip db & handler tests

--- resources/restaurant/get_test.go
+++ resources/restaurant/get_test.go
@@ -4,16 +4,20 @@ import (
   "testing"
 )
 
-// ...
+// TestGetAllHandler tests the get all handler by creating multiple fake
+// restaurant records and then fetching all of them by creating a mock GET
+// request.
 func TestGetAllHandler(t *testing.T) {
 
   // ...
 
 }
 
-// ...
+// TestGetOneHandler teste the get one handler by creating a fake restaurant
+// record and then fetching that record by its id - by creating a mock GET
+// request.
 func TestGetOneHandler(t *testing.T) {
 
   // ...
-  
+
 }
--- resources/restaurant/go.mod
+++ resources/restaurant/go.mod
@@ -3,9 +3,8 @@ module api-oaxaca-com/resources/restaurant
 go 1.13
 
 require (
-  api-oaxaca-com/packages/db v0.0.0
+	api-oaxaca-com/packages/db v0.0.0
+	github.com/gin-gonic/gin v1.5.0
 )
 
-replace (
-  api-oaxaca-com/packages/db => ../../packages/db
-)
+replace api-oaxaca-com/packages/db => ../../packages/db
