f0b6dce291bc0a1fe29272c025d56ecdd055ef23
Chung <ZFAC109@live.rhul.ac.uk>
2020-01-28 11:59:04 +0000

    Created boiler plate for tests in staff resource

--- resources/staff/db_test.go
+++ resources/staff/db_test.go
@@ -1,1 +1,52 @@
 package staff
+
+import (
+  "testing"
+)
+
+// TestCreateStaff tests the CreateStaff db method by creating a fake
+// staff record and then trying to retrieve it from the database. If these two
+// steps complete without error, we assume the test passes.
+func TestCreateStaff(t *testing.T) {
+
+  // ...
+
+}
+
+// TestGetStaff tests the GetStaff db method by creating a fake staff
+// record and then retrieving it by its id. If both of these steps complete
+// without error, we assume the test passes.
+func TestGetStaff(t *testing.T) {
+
+  // ...
+
+}
+
+// TestGetAllStaff tests the GetAllStaff db method by creating multiple
+// fake staff records and then retrieving all of those records. If both of these
+// steps complete without error, we assume the test passes.
+func TestGetAllStaff(t *testing.T) {
+
+  // ...
+
+}
+
+// TestUpdateStaff tests the UpdateStaff db method by creating a fake
+// staff record and then updating one of its fields. The updated record is
+// then retrieved from the database, and the updated field is checked. If these
+// steps complete without error, we assume the test passes.
+func TestUpdateStaff(t *testing.T) {
+
+  // ...
+
+}
+
+// TestDeleteStaff tests the DeleteStaff db method by creating a fake
+// record and then deleting it. Then tries to re-fetch the deleted record to
+// make sure it doesn't exist. If these steps complete without error, we assume
+// the test passes.
+func TestDeleteStaff(t *testing.T) {
+
+  // ...
+
+}
--- resources/staff/get_test.go
+++ resources/staff/get_test.go
@@ -1,1 +1,23 @@
 package staff
+
+import (
+  "testing"
+)
+
+// TestGetAllHandler tests the get all handler by creating multiple fake
+// staff records and then fetching all of them by creating a mock GET
+// request.
+func TestGetAllHandler(t *testing.T) {
+
+  // ...
+
+}
+
+// TestGetOneHandler tests the get one handler by creating a fake staff
+// record and then fetching that record by its id - by creating a mock GET
+// request.
+func TestGetOneHandler(t *testing.T) {
+
+  // ...
+
+}
