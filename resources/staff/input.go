8c3bd25595f5e8866fe085739f69de715daddf58
William <william.santos@hotmail.co.uk>
2020-02-13 12:12:47 +0000

    fix model and input fields

--- resources/staff/input.go
+++ resources/staff/input.go
@@ -5,28 +5,29 @@ package staff
 type createInput struct {
   RestaurantID int64 `json:"restaurant_id"`
   FullName string `json:"full_name"`
-  UserName string `json:"user_name"`
-  PasswordHash string `json:"password_hash"`
+  Score int `json:"score"`
+  Password string `json:"password"`
   HasPassedTraining bool `json:"has_passed_training"`
+  Role string `json:"role"`
 }
 
 // IsValid ensures the fields provided in createInput match a set of rules. If
 // validation fails, a reason is returned - which should be sent to the client.
 func (i *createInput) IsValid() (bool, string) {
-  if i.RestaurantID < 0 {
-    return false, "RestaurantID cannot be less than 0"
+  if len(i.FullName) < 2 || len(i.FullName) > 60 {
+    return false, "Length of full name must be between 2 and 60"
   }
 
-  if len(i.FullName) < 2 || len(i.FullName) > 40 {
-    return false, "Length of full name must be between 2 and 40"
+  if i.Score < 0 {
+    return false, "Score cannot be less than 0"
   }
 
-  if len(i.UserName) < 1 || len(i.UserName) > 20 {
-    return false, "Length of user name must be between 1 and 20"
+  if len(i.Password) < 6 || len(i.Password) > 32 {
+    return false, "Length of password hash must be between 6 and 32"
   }
 
-  if len(i.PasswordHash) < 1 || len(i.PasswordHash) > 16 {
-    return false, "Length of password hash must be between 1 and 16"
+  if len(i.Role) > 20 {
+    return false, "Length of role cannot be greater than 20"
   }
 
   return true, ""
@@ -36,30 +37,31 @@ func (i *createInput) IsValid() (bool, string) {
 // /staffs/{id} which are used to fully update or partially update a
 // staff record in the database.
 type updateInput struct {
-  RestaurantID int64 `json:"restaurant_id"`
-  FullName string `json:"full_name"`
-  UserName string `json:"user_name"`
-  PasswordHash string `json:"password_hash"`
-  HasPassedTraining bool `json:"has_passed_training"`
+  RestaurantID *int64 `json:"restaurant_id"`
+  FullName *string `json:"full_name"`
+  Score *int `json:"score"`
+  Password *string `json:"password"`
+  HasPassedTraining *bool `json:"has_passed_training"`
+  Role *string `json:"role"`
 }
 
 // IsValid ensures the fields provided in updateInput match a set of rules. If
 // validation fails, a reason is returned - which should be sent to the client.
 func (i *updateInput) IsValid() (bool, string) {
-  if i.RestaurantID < 0 {
-    return false, "RestaurantID cannot be less than 0"
+  if i.FullName != nil && (len(*i.FullName) < 2 || len(*i.FullName) > 60) {
+    return false, "Length of full name must be between 2 and 60"
   }
 
-  if len(i.FullName) < 2 || len(i.FullName) > 40 {
-    return false, "Length of full name must be between 2 and 40"
+  if i.Score != nil && *i.Score < 0 {
+    return false, "Score cannot be less than 0"
   }
 
-  if len(i.UserName) < 1 || len(i.UserName) > 20 {
-    return false, "Length of user name must be between 1 and 20"
+  if i.Password != nil && (len(*i.Password) < 6 || len(*i.Password) > 32) {
+    return false, "Length of password hash must be between 6 and 32"
   }
 
-  if len(i.PasswordHash) < 1 || len(i.PasswordHash) > 16 {
-    return false, "Length of password hash must be between 1 and 16"
+  if i.Role != nil && len(*i.Role) > 20 {
+    return false, "Length of role cannot be greater than 20"
   }
 
   return true, ""
